fb7049a2e70d208ada97776a5276f6c9d1320ea1
JulianDGP <juliandgp@hotmail.com>
2024-01-20 16:04:58 -0500

    creacion del controlador Login y creacion de los demas endpoints de la aplicacion

--- Backend/app/src/main/java/org/ensolvers/app/controllers/LoginController.java
+++ Backend/app/src/main/java/org/ensolvers/app/controllers/LoginController.java
@@ -2,12 +2,15 @@ package org.ensolvers.app.controllers;
 
 import org.ensolvers.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class LoginController {
     private final UserService userService;
@@ -18,7 +21,13 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
-        return userService.findUser(username, password);
+    public ResponseEntity<?> login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        String result = userService.findUser(username, password);
+        if (result.equals("Login successful")) {
+            return ResponseEntity.ok().body(result);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+        }
     }
+
 }
