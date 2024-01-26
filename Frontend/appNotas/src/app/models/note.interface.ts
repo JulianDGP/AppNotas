5a5fa9f1433ffb6d9f6c277855490f1fc2175bf9
JulianDGP <47646912+JulianDGP@users.noreply.github.com>
2024-01-25 23:42:50 -0500

    Refractor

--- Frontend/appNotas/src/app/models/note.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Tag } from "./tag.interface";
-export interface Note {
-    id: Number;
-    title: string;
-    content: string;
-    tags: Tag[];
-    // ... otros campos de la nota
-   }
--- Frontend/appNotas/src/app/models/tag.interface.ts
+++
@@ -1,6 +0,0 @@
-
-export interface Tag {
-    id:Number;
-    name: string;
-    // ... otros campos de la etiqueta
-}
