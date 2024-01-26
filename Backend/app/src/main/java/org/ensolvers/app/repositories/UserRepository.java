1bbec7e5bbdab201046e38a05f9e3f8535928af4
JulianDGP <47646912+JulianDGP@users.noreply.github.com>
2024-01-25 23:42:36 -0500

    refractor

--- Backend/app/src/main/java/org/ensolvers/app/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ensolvers.app.repositories;
-
-import org.ensolvers.app.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findUserByUserName(String username);
-}
